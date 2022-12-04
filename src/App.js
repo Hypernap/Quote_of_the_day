b430582ec6dbab1bb8d8ea6327f962ccd91266b3
Hypernap <sankalp.dubedy4@gmai.com>
2022-12-04 14:59:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
+
+
+const options = {
+  method: 'GET',
+  url:' https://api.api-ninjas.com/v1/quotes?category=happiness',
+  headers: {
+    'X-Api-Key': 'o+VeR1CuxdY8behyl2iKrg==U2l4BgTfAgbSvIDX'
+  }
+};
+axios.request(options).then(function (response) {
+	console.log(response.data['0']['quote']);
+}).catch(function (error) {
+	console.error(error);
+});
+console.log(options.response);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='card'>
+        <p>hhhhhhh</p>
+          <h3>HELLO</h3>
+          
+        </div>
+        <div className='btn1'>Next</div>
       </header>
     </div>
   );
